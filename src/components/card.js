76d90c003f47eb7fe26453d4f8bcb300b11f6904
Bethel Wilson <bethel.wilson@gmail.com>
2018-07-26 14:44:02 +0000

    testing

---
+++ src/components/card.js
@@ -0,0 +1,11 @@
+import React, { Component} from "react";
+
+class Card extends Component {
+    render() {
+        return (
+            <div className="card">
+                This is where our card inputs go.
+            </div>    
+        )
+    }
+}
