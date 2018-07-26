888317f59d427cee320a98fcf3934437c93eb3aa
Bethel Wilson <bethel.wilson@gmail.com>
2018-07-26 14:37:09 +0000

    built first functional component

--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 
-export default class Home extends Component {
+import Header from './header';
+
+class Home extends Component {
   render() {
     return (
       <div className="home">
-        
+        { Header() }
       </div>
     );
   }
 }
+
+export default Home;
