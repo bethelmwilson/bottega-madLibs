acfdb9c105b53913d376793afb5c79d5e0856975
Bethel Wilson <bethel.wilson@gmail.com>
2018-07-30 14:25:38 +0000

    creation of content file

---
+++ src/components/content.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Content extends Component {
+    reder() {
+        const data = this.props.data;
+        return (
+            <p>
+            </p>
+        )
+    }
+}
+
+export default Content;
