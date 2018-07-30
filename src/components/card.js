18c40730faa2f84aa0a6f1d70efea3b73df93890
Bethel Wilson <bethel.wilson@gmail.com>
2018-07-30 15:47:03 +0000

    clearing entered in data when hitting clear button

--- src/components/card.js
+++ src/components/card.js
@@ -1,48 +1,53 @@
-import React, { Component} from "react";
+import React, { Component } from 'react';
 
-import Input from "./input"
+import Input from './input';
 import Content from './content';
 
+const INITIAL_STATE = {
+    color: '',
+    pluralNoun: '',
+    adjectiveOne: '',
+    celebOne: '',
+    adjectiveTwo: '',
+    nounOne: '',
+    numberOne: '',
+    numberTwo: '',
+    nounTwo: '',
+    adjectiveThree: '',
+    celebTwo: '',
+    celebThree: '',
+    adjectiveFour: '',
+    nounThree: '',
+    celebFour: '',
+    adjectiveFive: '',
+    contentVisible: false
+}
+
 class Card extends Component {
-    
-      constructor() {
+
+    constructor() {
         super()
 
-        this.state = {
-            color: '',
-            pluralNoun: '',
-            adjectiveOne: '',
-            celebOne: '',
-            adjectiveTwo: '',
-            nounOne: '',
-            numberOne: '',
-            numberTwo: '',
-            nounTwo: '',
-            adjectiveThree: '',
-            celebTwo: '',
-            celebThree: '',
-            adjectiveFour: '',
-            nounThree: '',
-            celebFour: '',
-            adjectiveFive: '',
-            contentVisible: false
-        }
+        this.state = INITIAL_STATE;
 
-        this.handleInputChange = this.handleInputChange.bind(this)
+        this.handleInputChange = this.handleInputChange.bind(this);
         this.handleFormSubmit = this.handleFormSubmit.bind(this);
-    }
-    
-    
-    
+    } 
+
     handleInputChange(event) {
-        this.setState({ [event.target.name]: event.target.value });
+        this.setState({ [event.target.name]: event.target.value })
     }
-    
+
     handleFormSubmit(event) {
-        event.preventDefault();
-        this.setState({ contentVisible: !this.state.contentVisible });
+        event.preventDefault()
+
+        if(this.state.contentVisible) {
+            this.setState(INITIAL_STATE)
+        } else {
+            this.setState({ contentVisible: true })
+        }
     }
-    
+
     render() {
 
         const inputData = [
@@ -64,22 +69,24 @@ class Card extends Component {
             {title: 'Adjective', state: this.state.adjectiveFour, name: 'adjectiveFour'},
             {title: 'Noun', state: this.state.nounThree, name: 'nounThree'},
             {title: 'Celebrity', state: this.state.celebFour, name: 'celebFour'},
-            {title: 'Adjective', state: this.state.adjectiveFive, name: 'adjectiveFive'}
-        ]  
-            
+            {title: 'Adjective', state: this.state.adjectiveFive, name: 'adjectiveFive'},
+        ]
+
         return (
-            <form onSubmit={this.handleFormSubmit} className='card'>
-                <div className='card_inputs'>
+            <form onSubmit={this.handleFormSubmit} className="card">
+                <div className="card__inputs">
                 {
-                    inputData.map(data => Input( (data), this.handleInputChange ))
+                    inputData.map((data, index) => {
+                        return Input( (data), this.handleInputChange, index) 
+                    })
                 }
                 </div>
-                <button type='submit'>{!this.state.contentVisible ? "Generate Mad Lib" : "Clear Form"}</button> 
+                <button className={`card__${!this.state.contentVisible ? 'generate' : 'clear'}`} type="submit">{!this.state.contentVisible ? 'Generate Mad Lib' : 'Clear Form'}</button>
                 {
-                    this.state.contentVisible ? <Content data={this.state}/> : <div>hello</div>
+                    this.state.contentVisible ? <Content data={this.state}/> : ''
                 }
             </form>
-        );
+        )
     }
 }
 
