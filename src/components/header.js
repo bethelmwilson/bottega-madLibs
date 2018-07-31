c84019d15335019102e1dc490997aa334bed05be
Bethel Wilson <bethel.wilson@gmail.com>
2018-07-31 14:51:42 +0000

    added header, h1, and paragraph styles

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div className='header'> 
+        <div className='header'>
+            <div className="header_skew"></div>
+            <div className="header_border"></div>
             <h1>Mad Libs</h1>
             <p>Fill out the feilds below and click the generate button to see the Mad Lib story.</p>
         </div>
